3aec5384c1cbcf0639eccd2582fce0ccb965467e
Aitor Aguado <aaguado@theworkshop.com>
2023-04-05 16:53:04 +0200

    module name typo amend

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,11 +1,11 @@
 output "pool_name" {
   description = "Pool name"
-  value       = module.oidc.pool_name
+  value       = module.gh_oidc.pool_name
 }
 
 output "provider_name" {
   description = "Provider name"
-  value       = module.oidc.provider_name
+  value       = module.gh_oidc.provider_name
 }
 
 output "sa_email" {
