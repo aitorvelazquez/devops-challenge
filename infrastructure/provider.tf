f07c020b427c3037c8300b891aede44b056101ac
Aitor Aguado <aaguado@theworkshop.com>
2023-04-05 11:30:21 +0200

    Comment added

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    # Cloud Run support was added on 3.3.0
-    # google = ">= 3.3"
+    # FYI. Cloud Run support was added on 3.3.0
     google = {
       source  = "hashicorp/google"
       version = "4.59.0"
