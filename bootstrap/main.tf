ac09911f9cc9f9670d55e562e9bd7fc98d84775b
Aitor Aguado <aaguado@theworkshop.com>
2023-04-05 15:10:41 +0200

    Removing SA and related resources

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -7,30 +7,6 @@ data "terraform_remote_state" "tf_state" {
   }
 }
 
-# Enable the IAM service to be able to create a SA
-resource "google_project_service" "project" {
-  project = var.gcp_project_id
-  service = "iam.googleapis.com"
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-  disable_dependent_services = true
-}
-
-# Create a SA
-resource "google_service_account" "gar_service_account" {
-  account_id   = "gar-sa-devops-challenge"
-  display_name = "Service Account to work with Google Artifact Repositories"
-}
-
-# Grant permissions to the SA
-resource "google_project_iam_member" "gar_repoAdmin_binding" {
-  project = var.gcp_project_id
-  role    = "roles/artifactregistry.repoAdmin"
-  member  = "serviceAccount:${google_service_account.gar_service_account.email}"
-}
-
 # Create a Artifact Registry to store our app images. Must be created before hand to build/push images.
 resource "google_artifact_registry_repository" "devops-challenge" {
   location      = var.gcp_region
